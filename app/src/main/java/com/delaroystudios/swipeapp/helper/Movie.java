9f8e78b90dc5892c1327e1ebd3bdab1b0c8c0cdc
delaroy <bamoyk@yahoo.com>
2015-11-15 13:14:07 +0100

    Swipe
    Swipe

---
+++ app/src/main/java/com/delaroystudios/swipeapp/helper/Movie.java
@@ -0,0 +1,19 @@
+package com.delaroystudios.swipeapp.helper;
+
+/**
+ * Created by Shade on 11/12/2015.
+ */
+public class Movie {
+
+    public int id;
+    public String title;
+
+    public Movie() {
+    }
+
+    public Movie(int id, String title) {
+        this.title = title;
+        this.id = id;
+    }
+
+}
